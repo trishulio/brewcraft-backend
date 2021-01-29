12d2a531ece8419d5bb29e344b231e79b5b11af8
Rishab Manocha <rishab.manocha@outlook.com>
2021-01-28 22:57:43 -0800

    Added unit tests for the UnitEntity

---
+++ src/test/java/io/company/brewcraft/model/UnitEntityTest.java
@@ -0,0 +1,37 @@
+package io.company.brewcraft.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UnitEntityTest {
+
+    private UnitEntity unit;
+
+    @BeforeEach
+    public void init() {
+        unit = new UnitEntity();
+    }
+
+    @Test
+    public void testAllArgsConstructor() {
+        unit = new UnitEntity("KG", "Kilogram");
+        assertEquals("KG", unit.getSymbol());
+        assertEquals("Kilogram", unit.getName());
+    }
+
+    @Test
+    public void testAccessSymbol() {
+        assertNull(unit.getSymbol());
+        unit.setSymbol("KG");
+        assertEquals("KG", unit.getSymbol());
+    }
+
+    @Test
+    public void testAccessName() {
+        assertNull(unit.getName());
+        unit.setName("Kilogram");
+        assertEquals("Kilogram", unit.getName());
+    }
+}
