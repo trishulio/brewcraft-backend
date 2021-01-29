68dff2c06abfac69de5eb4d6e1c700425fa50eef
Rishab Manocha <rishab.manocha@outlook.com>
2021-01-28 22:47:03 -0800

    Added unit tests for currency

---
+++ src/test/java/io/company/brewcraft/model/CurrencyTest.java
@@ -0,0 +1,37 @@
+package io.company.brewcraft.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CurrencyTest {
+
+    private Currency currency;
+
+    @BeforeEach
+    public void init() {
+        currency = new Currency();
+    }
+
+    @Test
+    public void testAllArgsConstructor() {
+        currency = new Currency(1234, "CAD");
+        assertEquals(1234, currency.getNumericCode());
+        assertEquals("CAD", currency.getCode());
+    }
+
+    @Test
+    public void testAccessNumericCode() {
+        assertNull(currency.getNumericCode());
+        currency.setNumericCode(123);
+        assertEquals(123, currency.getNumericCode());
+    }
+
+    @Test
+    public void testAccessCode() {
+        assertNull(currency.getCode());
+        currency.setCode("USD");
+        assertEquals("USD", currency.getCode());
+    }
+}
