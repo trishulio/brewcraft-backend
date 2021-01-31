b1c7dd8db593ee1225ec6261eeaa39711011320e
Rishab Manocha <rishab.manocha@outlook.com>
2021-01-31 02:57:22 -0800

    Merged the po service changes

--- src/main/java/io/company/brewcraft/service/PurchaseOrderService.java
+++ src/main/java/io/company/brewcraft/service/PurchaseOrderService.java
@@ -1,9 +1,10 @@
 package io.company.brewcraft.service;
 
+import java.util.Optional;
+
 import io.company.brewcraft.model.PurchaseOrderEntity;
 import io.company.brewcraft.pojo.PurchaseOrder;
 import io.company.brewcraft.repository.PurchaseOrderRepository;
-import io.company.brewcraft.service.exception.EntityNotFoundException;
 import io.company.brewcraft.service.mapper.PurchaseOrderMapper;
 
 public class PurchaseOrderService {
@@ -16,7 +17,11 @@ public class PurchaseOrderService {
     }
 
     public PurchaseOrder getPurchaseOrder(Long id) {
-        PurchaseOrderEntity po = this.repo.findById(id).orElseThrow(() -> new EntityNotFoundException("PurchaseOrder", id.toString()));
-        return mapper.fromEntity(po);
+        PurchaseOrder po = null;
+        Optional<PurchaseOrderEntity> entity = this.repo.findById(id);
+        if (entity.isPresent()) {
+            po = mapper.fromEntity(entity.get());
+        }
+        return po;
     }
 }
