edffa6e5a6d3cefe6055072985df52f5656cb136
Rishab Manocha <rishab.manocha@outlook.com>
2021-01-30 20:33:23 -0800

    Added InvoiceServiceTest

---
+++ src/test/java/io/company/brewcraft/service/impl/InvoiceStatusServiceTest.java
@@ -0,0 +1,43 @@
+package io.company.brewcraft.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.company.brewcraft.model.InvoiceStatusEntity;
+import io.company.brewcraft.pojo.InvoiceStatus;
+import io.company.brewcraft.repository.InvoiceStatusRepository;
+import io.company.brewcraft.service.InvoiceStatusService;
+
+public class InvoiceStatusServiceTest {
+    private InvoiceStatusService service;
+
+    private InvoiceStatusRepository mRepo;
+
+    @BeforeEach
+    public void init() {
+        mRepo = mock(InvoiceStatusRepository.class);
+        service = new InvoiceStatusService(mRepo);
+    }
+
+    @Test
+    public void testGetInvoiceStatus_ReturnsPojo_WhenEntityExists() {
+        InvoiceStatusEntity mEntity = new InvoiceStatusEntity(1L, "FINAL");
+        doReturn(Optional.of(mEntity)).when(mRepo).findByName("FINAL");
+
+        InvoiceStatus status = service.getInvoiceStatus("FINAL");
+        assertEquals(new InvoiceStatus(1L, "FINAL"), status);
+    }
+
+    @Test
+    public void testGetInvoiceStatus_ReturnsNull_WhenEntityDoesNotExists() {
+        doReturn(Optional.empty()).when(mRepo).findByName("FINAL");
+
+        InvoiceStatus status = service.getInvoiceStatus("FINAL");
+        assertNull(status);
+    }
+}
