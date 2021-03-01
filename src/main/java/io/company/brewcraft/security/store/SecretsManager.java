ce0ee74f41c609308e9a89c9927ecbe51a3daed3
merschbamer <69827679+merschbamer@users.noreply.github.com>
2021-02-28 21:18:04 -0800

    Integration with AWS Secrets Manager (#30)

--- src/main/java/io/company/brewcraft/security/store/SecretsManager.java
+++ src/main/java/io/company/brewcraft/security/store/SecretsManager.java
@@ -4,8 +4,12 @@ import java.io.IOException;
 
 public interface SecretsManager<K, V> {
 
-    public V get(K secretId) throws IOException;
+    public V get(K secretId) throws IOException ;
 
     public void put(K secretId, V secret) throws IOException;
+    
+    public void create(K secretId, V secret) throws IOException;
+
+    public void update(K secretId, V secret) throws IOException;
 
 }
