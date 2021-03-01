4f18ee723e4c6adb3c8cc24ff359dbafcf16524a
Matthew Erschbamer <erschbombr@gmail.com>
2021-02-28 17:40:02 -0800

    Integration with AWS Secrets Manager

--- src/main/java/io/company/brewcraft/security/store/AwsSecretsManagerClient.java
+++ src/main/java/io/company/brewcraft/security/store/AwsSecretsManagerClient.java
@@ -14,7 +14,9 @@ import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
 import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
 import com.amazonaws.services.secretsmanager.model.InvalidParameterException;
 import com.amazonaws.services.secretsmanager.model.InvalidRequestException;
+import com.amazonaws.services.secretsmanager.model.PutSecretValueRequest;
 import com.amazonaws.services.secretsmanager.model.ResourceNotFoundException;
+import com.amazonaws.services.secretsmanager.model.UpdateSecretRequest;
 
 public class AwsSecretsManagerClient implements SecretsManager<String, String> {
 
@@ -22,25 +24,25 @@ public class AwsSecretsManagerClient implements SecretsManager<String, String> {
 
     private static final Logger logger = LoggerFactory.getLogger(AwsSecretsManagerClient.class);
 
-    public AwsSecretsManagerClient(@Value("${aws.secretsmanager.region:us-west-1}") String region, @Value("${aws.secretsmanager.url:http://localstack:4566}") String url) {
+    public AwsSecretsManagerClient(@Value("${aws.secretsmanager.region}") String region, @Value("${aws.secretsmanager.url}") String url) {
         AwsClientBuilder.EndpointConfiguration endpointConfig = new AwsClientBuilder.EndpointConfiguration(url, region);
         client = AWSSecretsManagerClientBuilder.standard().withEndpointConfiguration(endpointConfig).build();
     }
 
     @Override
     public String get(String secretId) throws IOException {
-        GetSecretValueRequest req = new GetSecretValueRequest().withSecretId(secretId);
-        GetSecretValueResult res = null;
+        GetSecretValueRequest getSecretRequest = new GetSecretValueRequest().withSecretId(secretId);
+        GetSecretValueResult result = null;
 
         try {
-            res = client.getSecretValue(req);
             String secret = null;
-            if (res != null && res.getSecretString() != null) {
-                secret = res.getSecretString();
+            result = client.getSecretValue(getSecretRequest);
+            if (result != null && result.getSecretString() != null) {
+                secret = result.getSecretString();
             }
-
+            
             return secret;
-
+            
         } catch (ResourceNotFoundException | InvalidRequestException | InvalidParameterException e) {
             logger.error("Error geting secret value for secretId: {}", secretId);
             throw new IOException(e);
@@ -49,12 +51,34 @@ public class AwsSecretsManagerClient implements SecretsManager<String, String> {
 
     @Override
     public void put(String secretId, String secret) throws IOException {
-        CreateSecretRequest req = new CreateSecretRequest().withName(secretId).withSecretString(secret);
+        PutSecretValueRequest updateSecretRequest = new PutSecretValueRequest().withSecretId(secretId).withSecretString(secret);
 
         try {
-            client.createSecret(req);
-        } catch (ResourceNotFoundException | InvalidRequestException | InvalidParameterException e) {
+            client.putSecretValue(updateSecretRequest);
+        } catch (InvalidRequestException | InvalidParameterException e) {
+            logger.error("Error updating secret with secretId: {}", secretId);
+            throw new IOException(e);
+        }
+    }
+    
+    public void create(String secretId, String secret) throws IOException {    
+        CreateSecretRequest createSecretRequest = new CreateSecretRequest().withName(secretId).withSecretString(secret);
+
+        try {
+            client.createSecret(createSecretRequest);
+        } catch (InvalidRequestException | InvalidParameterException e) {
             logger.error("Error createing secret with secretId: {}", secretId);
+            throw new IOException(e);
+        }
+    }
+    
+    public void update(String secretId, String secret) throws IOException { 
+        UpdateSecretRequest updateSecretRequest = new UpdateSecretRequest().withSecretId(secretId).withSecretString(secret);
+
+        try {
+            client.updateSecret(updateSecretRequest);
+        } catch (InvalidRequestException | InvalidParameterException e) {
+            logger.error("Error updating secret with secretId: {}", secretId);
             throw new IOException(e);
         }
     }
