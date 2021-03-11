df0fc6f6035dd9b2bf766d39244315e543a0e948
Rishab Manocha <rishab.manocha@outlook.com>
2021-03-10 21:49:39 -0800

    Refactored the QtyUnitMapper Test to have strict and clear contract

--- src/test/java/io/company/brewcraft/service/mapper/QuantityUnitMapperTest.java
+++ src/test/java/io/company/brewcraft/service/mapper/QuantityUnitMapperTest.java
@@ -3,9 +3,6 @@ package io.company.brewcraft.service.mapper;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.measure.Unit;
 import javax.measure.quantity.AmountOfSubstance;
 
@@ -35,49 +32,46 @@ public class QuantityUnitMapperTest {
 
     @Test
     public void testFromSymbol_ReturnsPojoMatchingSymbol_WhenSymbolIsNotNull() {
-        Map<String, Unit<?>> symbolsMap = new HashMap<>();
-        symbolsMap.put(Units.AMPERE.toString(), Units.AMPERE);
-        symbolsMap.put(Units.BECQUEREL.toString(), Units.BECQUEREL);
-        symbolsMap.put(Units.CANDELA.toString(), Units.CANDELA);
-        symbolsMap.put(Units.CELSIUS.toString(), Units.CELSIUS);
-        symbolsMap.put(Units.COULOMB.toString(), Units.COULOMB);
-        symbolsMap.put(Units.CUBIC_METRE.toString(), Units.CUBIC_METRE);
-        symbolsMap.put(Units.DAY.toString(), Units.DAY);
-        symbolsMap.put(Units.FARAD.toString(), Units.FARAD);
-        symbolsMap.put(Units.GRAM.toString(), Units.GRAM);
-        symbolsMap.put(Units.GRAY.toString(), Units.GRAY);
-        symbolsMap.put(Units.HENRY.toString(), Units.HENRY);
-        symbolsMap.put(Units.HERTZ.toString(), Units.HERTZ);
-        symbolsMap.put(Units.HOUR.toString(), Units.HOUR);
-        symbolsMap.put(Units.JOULE.toString(), Units.JOULE);
-        symbolsMap.put(Units.KATAL.toString(), Units.KATAL);
-        symbolsMap.put(Units.KELVIN.toString(), Units.KELVIN);
-        symbolsMap.put(Units.KILOGRAM.toString(), Units.KILOGRAM);
-        symbolsMap.put(Units.KILOMETRE_PER_HOUR.toString(), Units.KILOMETRE_PER_HOUR);
-        symbolsMap.put(Units.LITRE.toString(), Units.LITRE);
-        symbolsMap.put(Units.LUMEN.toString(), Units.LUMEN);
-        symbolsMap.put(Units.LUX.toString(), Units.LUX);
-        symbolsMap.put(Units.METRE.toString(), Units.METRE);
-        symbolsMap.put(Units.METRE_PER_SECOND.toString(), Units.METRE_PER_SECOND);
-        symbolsMap.put(Units.MINUTE.toString(), Units.MINUTE);
-        symbolsMap.put(Units.MOLE.toString(), Units.MOLE);
-        symbolsMap.put(Units.NEWTON.toString(), Units.NEWTON);
-        symbolsMap.put(Units.OHM.toString(), Units.OHM);
-        symbolsMap.put(Units.PERCENT.toString(), Units.PERCENT);
-        symbolsMap.put(Units.RADIAN.toString(), Units.RADIAN);
-        symbolsMap.put(Units.SECOND.toString(), Units.SECOND);
-        symbolsMap.put(Units.SIEMENS.toString(), Units.SIEMENS);
-        symbolsMap.put(Units.SIEVERT.toString(), Units.SIEVERT);
-        symbolsMap.put(Units.SQUARE_METRE.toString(), Units.SQUARE_METRE);
-        symbolsMap.put(Units.STERADIAN.toString(), Units.STERADIAN);
-        symbolsMap.put(Units.TESLA.toString(), Units.TESLA);
-        symbolsMap.put(Units.VOLT.toString(), Units.VOLT);
-        symbolsMap.put(Units.WATT.toString(), Units.WATT);
-        symbolsMap.put(Units.WEBER.toString(), Units.WEBER);
-        symbolsMap.put(Units.WEEK.toString(), Units.WEEK);
-        symbolsMap.put(Units.YEAR.toString(), Units.YEAR);
-
-        symbolsMap.forEach((symbol, unit) -> assertSame(unit, mapper.fromSymbol(symbol)));
+        assertSame(Units.AMPERE, mapper.fromSymbol("A"));
+        assertSame(Units.BECQUEREL, mapper.fromSymbol(Units.BECQUEREL.toString()));
+        assertSame(Units.CANDELA, mapper.fromSymbol("cd"));
+        assertSame(Units.CELSIUS, mapper.fromSymbol("°C"));
+        assertSame(Units.COULOMB, mapper.fromSymbol("C"));
+        assertSame(Units.CUBIC_METRE, mapper.fromSymbol("㎥"));
+        assertSame(Units.DAY, mapper.fromSymbol("day"));
+        assertSame(Units.FARAD, mapper.fromSymbol("F"));
+        assertSame(Units.GRAM, mapper.fromSymbol("g"));
+        assertSame(Units.GRAY, mapper.fromSymbol("Gy"));
+        assertSame(Units.HENRY, mapper.fromSymbol("H"));
+        assertSame(Units.HERTZ, mapper.fromSymbol("Hz"));
+        assertSame(Units.HOUR, mapper.fromSymbol("h"));
+        assertSame(Units.JOULE, mapper.fromSymbol("J"));
+        assertSame(Units.KATAL, mapper.fromSymbol("kat"));
+        assertSame(Units.KELVIN, mapper.fromSymbol("K"));
+        assertSame(Units.KILOGRAM, mapper.fromSymbol("kg"));
+        assertSame(Units.KILOMETRE_PER_HOUR, mapper.fromSymbol("km/h"));
+        assertSame(Units.LITRE, mapper.fromSymbol("l"));
+        assertSame(Units.LUMEN, mapper.fromSymbol("lm"));
+        assertSame(Units.LUX, mapper.fromSymbol("lx"));
+        assertSame(Units.METRE, mapper.fromSymbol("m"));
+        assertSame(Units.METRE_PER_SECOND, mapper.fromSymbol("m/s"));
+        assertSame(Units.MINUTE, mapper.fromSymbol("min"));
+        assertSame(Units.MOLE, mapper.fromSymbol("mol"));
+        assertSame(Units.NEWTON, mapper.fromSymbol("N"));
+        assertSame(Units.OHM, mapper.fromSymbol("Ω"));
+        assertSame(Units.PERCENT, mapper.fromSymbol("%"));
+        assertSame(Units.RADIAN, mapper.fromSymbol("rad"));
+        assertSame(Units.SECOND, mapper.fromSymbol("s"));
+        assertSame(Units.SIEMENS, mapper.fromSymbol("S"));
+        assertSame(Units.SIEVERT, mapper.fromSymbol("Sv"));
+        assertSame(Units.SQUARE_METRE, mapper.fromSymbol("m²"));
+        assertSame(Units.STERADIAN, mapper.fromSymbol("sr"));
+        assertSame(Units.TESLA, mapper.fromSymbol("T"));
+        assertSame(Units.VOLT, mapper.fromSymbol("V"));
+        assertSame(Units.WATT, mapper.fromSymbol("W"));
+        assertSame(Units.WEBER, mapper.fromSymbol("Wb"));
+        assertSame(Units.WEEK, mapper.fromSymbol("week"));
+        assertSame(Units.YEAR, mapper.fromSymbol("year"));
 
         // Custom values tests
         assertEquals(new BaseUnit<AmountOfSubstance>("each", QuantityDimension.AMOUNT_OF_SUBSTANCE), mapper.fromSymbol("each"));
