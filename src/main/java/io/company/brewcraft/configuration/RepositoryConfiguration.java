9eaa54626d57dc5142680e295e1f28ba342ec603
Rishab Manocha <rishab.manocha@outlook.com>
2021-04-27 19:42:05 -0700

    Added Repo config

---
+++ src/main/java/io/company/brewcraft/configuration/RepositoryConfiguration.java
@@ -0,0 +1,94 @@
+package io.company.brewcraft.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.company.brewcraft.model.InvoiceItem;
+import io.company.brewcraft.model.InvoiceStatus;
+import io.company.brewcraft.model.Material;
+import io.company.brewcraft.model.MaterialLot;
+import io.company.brewcraft.model.PurchaseOrder;
+import io.company.brewcraft.model.ShipmentStatus;
+import io.company.brewcraft.model.Storage;
+import io.company.brewcraft.repository.AccessorRefresher;
+import io.company.brewcraft.repository.InvoiceItemRepository;
+import io.company.brewcraft.repository.InvoiceStatusRepository;
+import io.company.brewcraft.repository.MaterialLotRepository;
+import io.company.brewcraft.repository.MaterialRepository;
+import io.company.brewcraft.repository.PurchaseOrderRepository;
+import io.company.brewcraft.repository.ShipmentStatusRepository;
+import io.company.brewcraft.repository.StorageRepository;
+import io.company.brewcraft.service.InvoiceItemAccessor;
+import io.company.brewcraft.service.InvoiceStatusAccessor;
+import io.company.brewcraft.service.MaterialAccessor;
+import io.company.brewcraft.service.MaterialLotAccessor;
+import io.company.brewcraft.service.PurchaseOrderAccessor;
+import io.company.brewcraft.service.ShipmentStatusAccessor;
+import io.company.brewcraft.service.StorageAccessor;
+
+@Configuration
+public class RepositoryConfiguration {
+
+    @Bean
+    public AccessorRefresher<Long, InvoiceItemAccessor, InvoiceItem> invoiceItemRefresher(InvoiceItemRepository repo) {
+        return new AccessorRefresher<>(
+            accessor -> accessor.getInvoiceItem(),
+            (accessor, invoiceItem) -> accessor.setInvoiceItem(invoiceItem),
+            ids -> repo.findAllById(ids)
+        );
+    }
+
+    @Bean
+    public AccessorRefresher<String, InvoiceStatusAccessor, InvoiceStatus> invoiceStatusRefresher(InvoiceStatusRepository repo) {
+        return new AccessorRefresher<>(
+            accessor -> accessor.getStatus(),
+            (accessor, status) -> accessor.setStatus(status),
+            names -> repo.findByNames(names)
+        );
+    }
+
+    @Bean
+    public AccessorRefresher<Long, MaterialLotAccessor, MaterialLot> materialLotRefresher(MaterialLotRepository repo) {
+        return new AccessorRefresher<>(
+            accessor -> accessor.getMaterialLot(),
+            (accessor, lot) -> accessor.setMaterialLot(lot),
+            ids -> repo.findAllById(ids)
+        );
+    }
+
+    @Bean
+    public AccessorRefresher<Long, MaterialAccessor, Material> materialRefresher(MaterialRepository repo) {
+        return new AccessorRefresher<>(
+            accessor -> accessor.getMaterial(),
+            (accessor, material) -> accessor.setMaterial(material),
+            ids -> repo.findAllById(ids)
+        );
+    }
+
+    @Bean
+    public AccessorRefresher<Long, PurchaseOrderAccessor, PurchaseOrder> purchaseOrderRefresher(PurchaseOrderRepository repo) {
+        return new AccessorRefresher<>(
+            accessor -> accessor.getPurchaseOrder(),
+            (accessor, purchaseOrder) -> accessor.setPurchaseOrder(purchaseOrder),
+            ids -> repo.findAllById(ids)
+        );
+    }
+
+    @Bean
+    public AccessorRefresher<String, ShipmentStatusAccessor, ShipmentStatus> shipmentStatusRefresher(ShipmentStatusRepository repo) {
+        return new AccessorRefresher<>(
+            accessor -> accessor.getStatus(),
+            (accessor, status) -> accessor.setStatus(status),
+            names -> repo.findByNames(names)
+        );
+    }
+
+    @Bean
+    public AccessorRefresher<Long, StorageAccessor, Storage> storageRefresher(StorageRepository repo) {
+        return new AccessorRefresher<>(
+            accessor -> accessor.getStorage(),
+            (accessor, status) -> accessor.setStorage(status),
+            names -> repo.findAllById(names)
+        );
+    }
+}
