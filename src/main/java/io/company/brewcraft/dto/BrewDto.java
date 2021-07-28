e7196c531fc6d26b09796f4cf66bfbdac259fe57
merschbamer <69827679+merschbamer@users.noreply.github.com>
2021-07-27 17:58:56 -0700

    Update src/main/java/io/company/brewcraft/dto/BrewDto.java
    Co-authored-by: Rishab Manocha <mrishab@users.noreply.github.com>

--- src/main/java/io/company/brewcraft/dto/BrewDto.java
+++ src/main/java/io/company/brewcraft/dto/BrewDto.java
@@ -6,9 +6,9 @@ public class BrewDto extends BaseDto {
     
     private Long id;
     
-	private String name;
-	
-	private String description;
+    private String name;
+
+    private String description;
     
     private Long batchId;
     
