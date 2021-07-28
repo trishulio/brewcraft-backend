50d3b690a3575f2ac670409b90ba7dafafc03091
Matthew Erschbamer <erschbombr@gmail.com>
2021-07-27 18:14:31 -0700

    Fix formatting

--- src/main/java/io/company/brewcraft/dto/BrewStageDto.java
+++ src/main/java/io/company/brewcraft/dto/BrewStageDto.java
@@ -3,95 +3,95 @@ package io.company.brewcraft.dto;
 import java.time.LocalDateTime;
 
 public class BrewStageDto extends BaseDto {
-    
-    private Long id;
-    
-    private Long brewId;
-    
-    private BrewStageStatusDto status;
-    
-    private BrewTaskDto task;
-                    
-    private LocalDateTime startedAt;
-    
-    private LocalDateTime endedAt;
 
-    private Integer version;
-    
-    public BrewStageDto() {
-    	super();
-    }
-    
-    public BrewStageDto(Long id) {
-    	this();
-    	this.id = id;
-    }
+	private Long id;
 
-    public BrewStageDto(Long id, Long brewId, BrewStageStatusDto status, BrewTaskDto task, LocalDateTime startedAt, LocalDateTime endedAt,
-            Integer version) {
-    	this(id);
-        this.brewId = brewId;
-        this.status = status;
-        this.task = task;
-        this.startedAt = startedAt;
-        this.endedAt = endedAt;
-        this.version = version;
-    }
+	private Long brewId;
 
-    public Long getId() {
-        return id;
-    }
+	private BrewStageStatusDto status;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-    
-    public Long getBrewId() {
-        return brewId;
-    }
+	private BrewTaskDto task;
 
-    public void setBrewId(Long brewId) {
-        this.brewId = brewId;
-    }
-    
-    public BrewStageStatusDto getStatus() {
-        return status;
-    }
+	private LocalDateTime startedAt;
 
-    public void setStatus(BrewStageStatusDto status) {
-        this.status = status;
-    }
+	private LocalDateTime endedAt;
 
-    public BrewTaskDto getTask() {
-        return task;
-    }
+	private Integer version;
 
-    public void setTask(BrewTaskDto task) {
-        this.task = task;
-    }
+	public BrewStageDto() {
+		super();
+	}
 
-    public LocalDateTime getStartedAt() {
-        return startedAt;
-    }
+	public BrewStageDto(Long id) {
+		this();
+		this.id = id;
+	}
 
-    public void setStartedAt(LocalDateTime startedAt) {
-        this.startedAt = startedAt;
-    }
+	public BrewStageDto(Long id, Long brewId, BrewStageStatusDto status, BrewTaskDto task, LocalDateTime startedAt,
+			LocalDateTime endedAt, Integer version) {
+		this(id);
+		this.brewId = brewId;
+		this.status = status;
+		this.task = task;
+		this.startedAt = startedAt;
+		this.endedAt = endedAt;
+		this.version = version;
+	}
 
-    public LocalDateTime getEndedAt() {
-        return endedAt;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setEndedAt(LocalDateTime endedAt) {
-        this.endedAt = endedAt;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public Integer getVersion() {
-        return version;
-    }
+	public Long getBrewId() {
+		return brewId;
+	}
 
-    public void setVersion(Integer version) {
-        this.version = version;
-    }
+	public void setBrewId(Long brewId) {
+		this.brewId = brewId;
+	}
+
+	public BrewStageStatusDto getStatus() {
+		return status;
+	}
+
+	public void setStatus(BrewStageStatusDto status) {
+		this.status = status;
+	}
+
+	public BrewTaskDto getTask() {
+		return task;
+	}
+
+	public void setTask(BrewTaskDto task) {
+		this.task = task;
+	}
+
+	public LocalDateTime getStartedAt() {
+		return startedAt;
+	}
+
+	public void setStartedAt(LocalDateTime startedAt) {
+		this.startedAt = startedAt;
+	}
+
+	public LocalDateTime getEndedAt() {
+		return endedAt;
+	}
+
+	public void setEndedAt(LocalDateTime endedAt) {
+		this.endedAt = endedAt;
+	}
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
 
 }
