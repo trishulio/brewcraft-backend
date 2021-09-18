b77bbb8a3f00b2ff66ed1dc850b4670f403362bc
Rishab Manocha <rishab.manocha@outlook.com>
2021-09-17 22:15:04 -0700

    Included support for sorting by Material Name

--- src/main/java/io/company/brewcraft/service/LotAggregationService.java
+++ src/main/java/io/company/brewcraft/service/LotAggregationService.java
@@ -1,10 +1,10 @@
 package io.company.brewcraft.service;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.Set;
 import java.util.SortedSet;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.domain.Specification;
 
@@ -15,7 +15,6 @@ import io.company.brewcraft.model.Shipment;
 import io.company.brewcraft.model.StockLot;
 import io.company.brewcraft.model.Storage;
 import io.company.brewcraft.repository.SpecificationBuilder;
-import static io.company.brewcraft.util.CollectionUtils.addToArray;
 
 /**
  * Gotcha: The service doesn't support sorting the results by Material Name. By the nature of the API call,
@@ -40,67 +39,74 @@ public class LotAggregationService {
     }
 
     public Page<ProcurementLot> getAggregatedProcurementQuantity(
-            Set<Long> ids,
-            Set<Long> excludeIds,
-            Set<String> lotNumbers,
-            Set<Long> materialIds,
-            Set<Long> shipmentIds,
-            Set<Long> storageIds,
-            Set<String> shipmentNumbers,
-            LocalDateTime deliveredDateFrom,
-            LocalDateTime deliveredDateTo,
-            AggregationFunction aggrFn,
-            ProcurementLot.AggregationField[] groupBy,
-            SortedSet<String> sort,
-            boolean orderAscending,
-            int page,
-            int size
-        ) {
-            final Specification<ProcurementLot> spec = SpecificationBuilder.builder()
-                                                                           .in(Lot.FIELD_ID, ids)
-                                                                           .not().in(Lot.FIELD_ID, excludeIds)
-                                                                           .in(Lot.FIELD_LOT_NUMBER, lotNumbers)
-                                                                           .in(new String[] { Lot.FIELD_MATERIAL, Material.FIELD_ID }, materialIds)
-                                                                           .in(new String[] { Lot.FIELD_SHIPMENT, Shipment.FIELD_ID }, shipmentIds)
-                                                                           .in(new String[] { Lot.FIELD_STORAGE, Storage.FIELD_ID }, storageIds)
-                                                                           .in(new String[] { Lot.FIELD_SHIPMENT, Shipment.FIELD_SHIPMENT_NUMBER }, shipmentNumbers)
-                                                                           .between(new String[] { Lot.FIELD_SHIPMENT, Shipment.FIELD_DELIVERED_DATE }, deliveredDateFrom, deliveredDateTo)
-                                                                           .build();
+        Set<Long> ids,
+        Set<Long> excludeIds,
+        Set<String> lotNumbers,
+        Set<Long> materialIds,
+        Set<Long> shipmentIds,
+        Set<Long> storageIds,
+        Set<String> shipmentNumbers,
+        LocalDateTime deliveredDateFrom,
+        LocalDateTime deliveredDateTo,
+        AggregationFunction aggrFn,
+        ProcurementLot.AggregationField[] groupBy,
+        SortedSet<String> sort,
+        boolean orderAscending,
+        int page,
+        int size
+    ) {
+        final Specification<ProcurementLot> spec = SpecificationBuilder.builder()
+                                                                       .in(Lot.FIELD_ID, ids)
+                                                                       .not().in(Lot.FIELD_ID, excludeIds)
+                                                                       .in(Lot.FIELD_LOT_NUMBER, lotNumbers)
+                                                                       .in(new String[] { Lot.FIELD_MATERIAL, Material.FIELD_ID }, materialIds)
+                                                                       .in(new String[] { Lot.FIELD_SHIPMENT, Shipment.FIELD_ID }, shipmentIds)
+                                                                       .in(new String[] { Lot.FIELD_STORAGE, Storage.FIELD_ID }, storageIds)
+                                                                       .in(new String[] { Lot.FIELD_SHIPMENT, Shipment.FIELD_SHIPMENT_NUMBER }, shipmentNumbers)
+                                                                       .between(new String[] { Lot.FIELD_SHIPMENT, Shipment.FIELD_DELIVERED_DATE }, deliveredDateFrom, deliveredDateTo)
+                                                                       .build();
 
-            groupBy = addToArray(groupBy, ProcurementLot.AggregationField.QUANTITY_UNIT);
-
-            return this.aggrService.getAggregation(ProcurementLot.class, spec, aggrFn, ProcurementLot.AggregationField.QUANTITY_VALUE, groupBy, sort, orderAscending, page, size);
+        if (ArrayUtils.contains(groupBy, ProcurementLot.AggregationField.MATERIAL)) {
+            groupBy = ArrayUtils.addAll(groupBy, ProcurementLot.AggregationField.MATERIAL_NAME, ProcurementLot.AggregationField.QUANTITY_UNIT);
+        } else {
+            groupBy = ArrayUtils.addAll(groupBy, ProcurementLot.AggregationField.QUANTITY_UNIT);
+        }
+        return this.aggrService.getAggregation(ProcurementLot.class, spec, aggrFn, ProcurementLot.AggregationField.QUANTITY_VALUE, groupBy, sort, orderAscending, page, size);
     }
 
     public Page<StockLot> getAggregatedStockQuantity(
-            Set<Long> ids,
-            Set<Long> excludeIds,
-            Set<String> lotNumbers,
-            Set<Long> materialIds,
-            Set<Long> shipmentIds,
-            Set<Long> storageIds,
-            Set<String> shipmentNumbers,
-            LocalDateTime deliveredDateFrom,
-            LocalDateTime deliveredDateTo,
-            AggregationFunction aggrFn,
-            StockLot.AggregationField[] groupBy,
-            SortedSet<String> sort,
-            boolean orderAscending,
-            int page,
-            int size
-        ) {
-            final Specification<StockLot> spec = SpecificationBuilder.builder()
-                                                                     .in(Lot.FIELD_ID, ids)
-                                                                     .not().in(Lot.FIELD_ID, excludeIds)
-                                                                     .in(Lot.FIELD_LOT_NUMBER, lotNumbers)
-                                                                     .in(new String[] { Lot.FIELD_MATERIAL, Material.FIELD_ID }, materialIds)
-                                                                     .in(new String[] { Lot.FIELD_SHIPMENT, Shipment.FIELD_ID }, shipmentIds)
-                                                                     .in(new String[] { Lot.FIELD_STORAGE, Storage.FIELD_ID }, storageIds)
-                                                                     .in(new String[] { Lot.FIELD_SHIPMENT, Shipment.FIELD_SHIPMENT_NUMBER }, shipmentNumbers)
-                                                                     .between(new String[] { Lot.FIELD_SHIPMENT, Shipment.FIELD_DELIVERED_DATE }, deliveredDateFrom, deliveredDateTo)
-                                                                     .build();
+        Set<Long> ids,
+        Set<Long> excludeIds,
+        Set<String> lotNumbers,
+        Set<Long> materialIds,
+        Set<Long> shipmentIds,
+        Set<Long> storageIds,
+        Set<String> shipmentNumbers,
+        LocalDateTime deliveredDateFrom,
+        LocalDateTime deliveredDateTo,
+        AggregationFunction aggrFn,
+        StockLot.AggregationField[] groupBy,
+        SortedSet<String> sort,
+        boolean orderAscending,
+        int page,
+        int size
+    ) {
+        final Specification<StockLot> spec = SpecificationBuilder.builder()
+                                                                 .in(Lot.FIELD_ID, ids)
+                                                                 .not().in(Lot.FIELD_ID, excludeIds)
+                                                                 .in(Lot.FIELD_LOT_NUMBER, lotNumbers)
+                                                                 .in(new String[] { Lot.FIELD_MATERIAL, Material.FIELD_ID }, materialIds)
+                                                                 .in(new String[] { Lot.FIELD_SHIPMENT, Shipment.FIELD_ID }, shipmentIds)
+                                                                 .in(new String[] { Lot.FIELD_STORAGE, Storage.FIELD_ID }, storageIds)
+                                                                 .in(new String[] { Lot.FIELD_SHIPMENT, Shipment.FIELD_SHIPMENT_NUMBER }, shipmentNumbers)
+                                                                 .between(new String[] { Lot.FIELD_SHIPMENT, Shipment.FIELD_DELIVERED_DATE }, deliveredDateFrom, deliveredDateTo)
+                                                                 .build();
 
-        groupBy = addToArray(groupBy, StockLot.AggregationField.QUANTITY_UNIT);
+        if (ArrayUtils.contains(groupBy, StockLot.AggregationField.MATERIAL)) {
+            groupBy = ArrayUtils.addAll(groupBy, StockLot.AggregationField.MATERIAL_NAME, StockLot.AggregationField.QUANTITY_UNIT);
+        } else {
+            groupBy = ArrayUtils.addAll(groupBy, StockLot.AggregationField.QUANTITY_UNIT);
+        }
 
         return this.aggrService.getAggregation(StockLot.class, spec, aggrFn, StockLot.AggregationField.QUANTITY_VALUE, groupBy, sort, orderAscending, page, size);
     }
