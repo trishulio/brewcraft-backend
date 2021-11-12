09fa43516c02da179786b0f7c5ebfbc3de8b5ca4
merschbamer <69827679+merschbamer@users.noreply.github.com>
2021-11-11 21:49:43 -0800

    Update src/test/java/io/company/brewcraft/service/impl/SimpleUpdateServiceTest.java
    Co-authored-by: Rishab Manocha <mrishab@users.noreply.github.com>

--- src/test/java/io/company/brewcraft/service/impl/SimpleUpdateServiceTest.java
+++ src/test/java/io/company/brewcraft/service/impl/SimpleUpdateServiceTest.java
@@ -3,8 +3,6 @@ package io.company.brewcraft.service.impl;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
