0c1c1f639c5e4766f597691a0237b66b035ac871
Matthew Erschbamer <erschbombr@gmail.com>
2021-11-29 17:13:57 -0800

    cleanup

--- src/main/java/io/company/brewcraft/model/BaseSku.java
+++ src/main/java/io/company/brewcraft/model/BaseSku.java
@@ -1,23 +1,17 @@
 package io.company.brewcraft.model;
 
-import java.util.List;
-
 import io.company.brewcraft.service.ProductAccessor;
 import io.company.brewcraft.service.SkuMaterialAccessor;
 
 public interface BaseSku<T extends BaseSkuMaterial<? extends BaseSku<T>>> extends ProductAccessor, QuantityAccessor, SkuMaterialAccessor<T> {
     final String ATTR_MATERIALS = "materials";
 
-    public String getName();
+    String getName();
 
-    public void setName(String name);
+    void setName(String name);
 
-    public String getDescription();
+    String getDescription();
 
-    public void setDescription(String description);
-
-    public List<T> getMaterials();
-
-    public void setMaterials(List<T> materials);
+    void setDescription(String description);
 
 }
