caf015c9325c19b8d8816dd32e0d4c91a04d7b23
Rishab Manocha <rishab.manocha@outlook.com>
2022-04-10 21:35:35 -0700

    Removed redundant sparse maxCapacity fields from Equipment

--- src/main/java/io/company/brewcraft/service/mapper/EquipmentMapper.java
+++ src/main/java/io/company/brewcraft/service/mapper/EquipmentMapper.java
@@ -2,7 +2,6 @@ package io.company.brewcraft.service.mapper;
 
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.NullValueCheckStrategy;
 import org.mapstruct.factory.Mappers;
 
@@ -14,7 +13,7 @@ import io.company.brewcraft.dto.UpdateEquipmentDto;
 import io.company.brewcraft.model.Equipment;
 import io.company.brewcraft.model.Facility;
 
-@Mapper(uses = { QuantityMapper.class, QuantityUnitMapper.class, AddressMapper.class})
+@Mapper(uses = { QuantityMapper.class, QuantityUnitMapper.class, AddressMapper.class })
 public interface EquipmentMapper {
     EquipmentMapper INSTANCE = Mappers.getMapper(EquipmentMapper.class);
 
@@ -22,19 +21,14 @@ public interface EquipmentMapper {
 
     FacilityBaseDto facilityToFacilityBaseDto(Facility facility);
 
-    @Mapping(target = "maxCapacity", source = "maxCapacityInDisplayUnit")
     EquipmentDto toDto(Equipment equipment);
 
-    @Mapping(target = "maxCapacity", source = "maxCapacityInDisplayUnit")
     FacilityEquipmentDto toFacilityEquipmentDto(Equipment equipment);
 
-    @Mapping(target = "maxCapacityDisplayUnit", source = "maxCapacity.symbol")
     Equipment fromDto(AddEquipmentDto equipment);
 
     @BeanMapping(nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
-    @Mapping(target = "maxCapacityDisplayUnit", source = "maxCapacity.symbol")
     Equipment fromDto(UpdateEquipmentDto equipment);
 
-    @Mapping(target = "maxCapacityDisplayUnit", source = "maxCapacity.symbol")
     Equipment fromDto(FacilityEquipmentDto equipment);
 }
