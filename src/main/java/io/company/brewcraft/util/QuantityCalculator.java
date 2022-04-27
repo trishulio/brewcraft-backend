1d9b64a9bb9e2c9438d9d7f4c009ef4360075959
merschbamer <69827679+merschbamer@users.noreply.github.com>
2022-04-26 18:59:53 -0700

    Refactor MixtureMaterialPortionService (#264)
    Remove constraint enforcing each lotId must be unique.

--- src/main/java/io/company/brewcraft/util/QuantityCalculator.java
+++ src/main/java/io/company/brewcraft/util/QuantityCalculator.java
@@ -7,7 +7,6 @@ import javax.measure.quantity.Mass;
 import javax.measure.quantity.Volume;
 
 import io.company.brewcraft.model.BaseQuantityUnitAccessor;
-import io.company.brewcraft.service.exception.IncompatibleQuantityUnitException;
 import tec.uom.se.quantity.Quantities;
 
 public class QuantityCalculator {
@@ -32,6 +31,28 @@ public class QuantityCalculator {
             Quantity<AmountOfSubstance> amount2 = (Quantity<AmountOfSubstance>) q2;
 
             return amount1.subtract(amount2);
+        } else {
+            throw new RuntimeException("Unsupported quantity type");
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public Quantity<?> add(Quantity<?> q1, Quantity<?> q2) {
+        if (SupportedUnits.DEFAULT_MASS.isCompatible(q1.getUnit())) {
+            Quantity<Mass> mass1 = (Quantity<Mass>) q1;
+            Quantity<Mass> mass2 = (Quantity<Mass>) q2;
+
+            return mass1.add(mass2);
+        } else if (SupportedUnits.DEFAULT_VOLUME.isCompatible(q1.getUnit())) {
+            Quantity<Volume> volume1 = (Quantity<Volume>) q1;
+            Quantity<Volume> volume2 = (Quantity<Volume>) q2;
+
+            return volume1.add(volume2);
+        } else if (SupportedUnits.EACH.isCompatible(q1.getUnit())) {
+            Quantity<AmountOfSubstance> amount1 = (Quantity<AmountOfSubstance>) q1;
+            Quantity<AmountOfSubstance> amount2 = (Quantity<AmountOfSubstance>) q2;
+
+            return amount1.add(amount2);
         } else {
             throw new RuntimeException("Unsupported quantity type");
         }
