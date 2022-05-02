bfa44312a281648bf320458d37d82f10e0e71056
Rishab Manocha <rishab.manocha@outlook.com>
2022-05-01 22:35:45 -0700

    BugFix: Null currency for the tax value returned empty results
    Hibernate by default performed an inner join on the currency table. When the value is
    null in the invoice table, it results in empty result set. Using lazy fetch means
    that the join is not longer performed in the select clause for invoice and the currency value
    is fetched in a separate query.
    
    PS: This could cause side-effects elsewhere so we need to keep an eye out

--- src/main/java/io/company/brewcraft/model/MoneyEntity.java
+++ src/main/java/io/company/brewcraft/model/MoneyEntity.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
@@ -12,7 +13,7 @@ public class MoneyEntity extends BaseEntity {
     public static final String FIELD_CURRENCY = "currency";
     public static final String FIELD_AMOUNT = "amount";
 
-    @ManyToOne(optional = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "currency_id", referencedColumnName = "numeric_code")
     private Currency currency;
 
